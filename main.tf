0755f80b0b72c1b72f44e9841eed091195e2adc0
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-06-24 13:14:05 +0900

    add: vm_instance

--- main.tf
+++ main.tf
@@ -18,3 +18,20 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-medium"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
