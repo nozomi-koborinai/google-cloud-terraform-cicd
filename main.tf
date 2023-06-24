7064a38da840b02057b6d4fa17391ecfa54b3cf2
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-06-24 15:06:14 +0900

    add: variables

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "google" {
-  credentials = file("secret.json")
+  credentials = file(var.credentials_file)
 
-  project = "koborinai-test"
-  region  = "asia-northeast1"
-  zone    = "asia-northeast1-a"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "koborinai-test"
+}
+
+variable "credentials_file" {
+    default = "secret.json"
+}
+
+variable "region" {
+    default = "asia-northeast1"
+}
+
+variable "zone" {
+  default = "asia-northeast1-a"
+}
