56e41a9bcca3563642215c04fc01a23cc5303385
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-06-24 13:16:24 +0900

    update: vm_instance tags

--- main.tf
+++ main.tf
@@ -22,6 +22,7 @@ resource "google_compute_network" "vpc_network" {
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "e2-medium"
+  tags = ["web", "dev"]
 
   boot_disk {
     initialize_params {
