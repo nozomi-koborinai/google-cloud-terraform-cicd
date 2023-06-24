64bc5c5d43daa96c76fffd99da2654dd1f8c19cf
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-06-24 12:47:37 +0900

    add: vpc_network

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("secret.json")
+
+  project = "koborinai-test"
+  region  = "asia-northeast1"
+  zone    = "asia-northeast1-a"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
