7c9f036f6ac6550e555c41149a1f0b3784fde6b0
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-07-16 17:16:31 +0900

    fix: cloud build file

--- main.tf
+++ main.tf
@@ -8,8 +8,6 @@ terraform {
 }
 
 provider "google" {
-  credentials = file(var.credentials_file)
-
   project = var.project
   region  = "asia-northeast1"
   zone    = "asia-northeast1-a"
--- variables.tf
+++ variables.tf
@@ -1,7 +1,3 @@
 variable "project" {
   type = string
 }
-
-variable "credentials_file" {
-  type = string
-}
