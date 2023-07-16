37950f4140268ce9a12515b73a132ae387507630
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-07-16 18:08:53 +0900

    remove: google compute network

--- main.tf
+++ main.tf
@@ -14,25 +14,3 @@ provider "google" {
   region  = "asia-northeast1"
   zone    = "asia-northeast1-a"
 }
-
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
-}
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "e2-medium"
-  tags         = ["web", "dev"]
-
-  boot_disk {
-    initialize_params {
-      image = "cos-cloud/cos-stable"
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {
-    }
-  }
-}
